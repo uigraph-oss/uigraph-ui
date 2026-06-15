bb1277f7d8564d0b8c005a8d998ab5fe3d2a7668
Nazmus Sayad <87106526+NazmusSayad@users.noreply.github.com>
2026-06-15 23:23:42 +0600

    refactor: remove unused fields from MeAndOrg query in auth-store

--- src/store/auth-store/gql-v2.ts
+++ src/store/auth-store/gql-v2.ts
@@ -4,11 +4,9 @@ export const GET_ME_AND_ORG_V2 = graphql(`
   query MeAndOrg {
     me {
       userId
-      orgId
       email
       name
       login
-      kind
       role
       authProvider
     }
